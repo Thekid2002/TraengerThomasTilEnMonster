2064de31f23d04e7d1ee18fab9c7fcc4789eb887
Thomas Andersen <coolfatboy02@gmail.com>
2023-10-20 13:35:32 +0200

    Traenger

---
+++ index.js
@@ -0,0 +1,12 @@
+
+let traenger = true;
+let answer = document.getElementById("answer")
+
+if(traenger) {
+    answer.innerHTML = "JA";
+    answer.style.color = "green";
+}else {
+    answer.innerHTML = "NEJ";
+    answer.style.color = "red";
+}
+
